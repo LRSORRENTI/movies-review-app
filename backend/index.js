5b889b8b844dc14727060d9bdac329ece8adfd97
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-11 08:04:29 -0600

    successful mongodb connection, also going to 'http://localhost:5000/api/v1/movies' does successfully output hello world from movies.route.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import app from "./server";
+import app from "./server.js";
 import mongodb from 'mongodb';
 import dotenv from 'dotenv';
 
@@ -9,23 +9,42 @@ async function main() {
     // first call dotenv.config to load in env vars
     dotenv.config();
     
+    console.log("Connecting to MongoDB...");
+
     const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+    
     // above create an instance using the new keyword and 
     // pass in the database URI
 
     const port = process.env.PORT || 8000;
     // Also bring in the port from env file, else default 
     // to 8000
+    try {
 
     await client.connect();
     // await further execution until client connects
 
+    console.log("Successfully connected to MongoDB.");
+
     app.listen(port, () => {
-        console.log(`Server is running on port: ${port}`);
+        console.log('server is running on port' + port);
     // app.listen will start the server and listens for requests 
     // to that port
     
-    })
+        })
+    } catch(e) {
+        console.error("Error during server startup:", e);
+        process.exit(1);
+    }
 }
 
-main().catch(console.error);
+main().catch(e => {
+    console.error("Unhandled error:", e);
+    process.exit(1);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Application specific logging, throwing an error, or other logic here
+});
