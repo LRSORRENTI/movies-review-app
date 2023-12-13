24f54672a4bf660001edeaa073c608ee2fa9789f
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-13 08:07:12 -0600

    Added setup for a /review route for crud operations, crud operations, put post delete for reviews

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import MoviesController from './movies.controller.js';
-
+import ReviewsController from './reviews.controller.js';
 
 const router = express.Router();
 // access express router 
@@ -11,5 +11,13 @@ const router = express.Router();
 
 router.route('/').get(MoviesController.apiGetMovies);
 // now above, each request for '/' will call the api
+
+// below we add a route to /review, this will handle 
+// all the basic methods for crud operations 
+
+router.route('/review')
+      .post(ReviewsController.apiPostReview)
+      .put(ReviewsController.apiUpdateReview)
+      .delete(ReviewsController.apiDeleteReview)
 
 export default router;
