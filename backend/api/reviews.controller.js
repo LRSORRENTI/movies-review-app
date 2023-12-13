3df939e7144cf190cd772343d14055c08a6f661e
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-13 08:11:17 -0600

    added setup for reviews controller

---
+++ backend/api/reviews.controller.js
@@ -0,0 +1,17 @@
+import ReviewsDAO from '..dao/reviewsDAO.js';
+
+export default class ReviewsController {
+    static async apiPostReview(req, res, next) {
+        try {
+            const movieId = req.body.movie_id;
+            const review = req.body.review;
+            const userInfo = {
+                name: req.body.name,
+                _id: req.body.user_id
+            };
+            const date = new Date();
+
+            
+        }
+    }
+}
