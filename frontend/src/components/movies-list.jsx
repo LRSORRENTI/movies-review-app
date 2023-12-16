50b769e8225ef6e7ff8c79e2ed2998fb94627f73
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-16 09:04:00 -0600

    Added more functions for updating state in the movie-list.jsx file

--- frontend/src/components/movies-list.jsx
+++ frontend/src/components/movies-list.jsx
@@ -2,23 +2,18 @@ import React, {useState, useEffect} from "react";
 import MovieDataService from '../services/movies.js';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const MoviesList = (props) => {
+const MoviesList = props => {
     const [movies, setMovies] = useState([]);
     const [searchTitle, setSearchTitle] = useState("");
     const [searchRatings, setSearchRatings] = useState("");
     const [ratings, setRatings] = useState(["All Ratings"]);
 
-    useEffect(() => {
-        MovieDataService.getAll()
-        .then(res => {
-            console.log(res.data)
-            setMovies(res.data.movies)
-        })
-        .catch( error => {
-            console.log(error)
-        })
-    })
-}
+
+useEffect(() => {
+    retrieveMovies()
+    retrieveRatings()
+}, [])
+
 
 const retrieveMovies = () => {
     MovieDataService.getAll()
@@ -31,10 +26,36 @@ const retrieveMovies = () => {
     })
 }
 
-export default function MoviesList() {
-    return (
-        <div className="App">
-            Movies List
-        </div>
-    );
+const retrieveRatings = () => {
+    MovieDataService.getRatings()
+        .then(res => {
+            console.log(res.data)
+            setRatings(["All Ratings"].concat(res.data))
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    };
+
+const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
 };
+
+const onChnageSearchRating = (e) => {
+    const searchRating = e.target.value;
+    setSearchRatings(searchRating)
+};
+
+return (
+    <div className="App">
+        <Container>
+            
+        </Container>
+    </div>
+)
+
+};
+
+
+export default MoviesList;
