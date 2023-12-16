6a4ac17cc4fb469ff721bab6e24643ad66012e11
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-16 08:40:23 -0600

    Added setup for useState variables and their updater funcs

--- frontend/src/components/movies-list.jsx
+++ frontend/src/components/movies-list.jsx
@@ -1,4 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import MovieDataService from '../services/movies.js';
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const MoviesList = (props) => {
+    const [movies, setMovies] = useState([]);
+    const [searchTitle, setSearchTitle] = useState("");
+    const [searchRatings, setSearchRatings] = useState("");
+    const [ratings, setRatings] = useState(["All Ratings"]);
+
+}
+
 
 export default function MoviesList() {
     return (
