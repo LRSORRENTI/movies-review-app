5aabf6628733e97f9ba7cbeee80313cde9fbd508
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-16 08:28:16 -0600

    Added setup for finding by title, and adding a review in the services file

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -9,4 +9,15 @@ class MovieDataService {
     get(id) {
         return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`)
     }
+
+    find(query, by = "title", page = 0){
+        return axios.get(
+            `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
+        )
+    }
+
+    createReview(data) {
+        return axios.post(`http://localhost:5000/api/v1/movies/review`, data)
+    }
+
 }
