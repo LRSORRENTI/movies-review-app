e11e3e3ca3987d00994816bf8e3280fb7c983fbd
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-18 06:58:54 -0600

    Added setup for the movie component

--- frontend/src/components/movie.jsx
+++ frontend/src/components/movie.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MovieDataService from '../services/movies.js';
+import Link from 'react-router-dom';
 
-export default function Movie() {
+export default function Movie(props) {
+    
+    const [movie, setMovie] = useState({
+        id: null,
+        title: "",
+        rated: "",
+        reviews: []
+    })
+
     return(
         <div className="App">
             Movie
