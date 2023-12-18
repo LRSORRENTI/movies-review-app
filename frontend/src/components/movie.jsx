ee957dad5f33b54dea16b1dc56cfa7200a38fde7
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-18 07:04:14 -0600

    added state and updater function in main movie component

--- frontend/src/components/movie.jsx
+++ frontend/src/components/movie.jsx
@@ -4,6 +4,10 @@ import Link from 'react-router-dom';
 
 export default function Movie(props) {
     
+    // first define the movie state var to house 
+    // the specific movie we currently show in the 
+    // Movie component, and useState to update the value,
+    // intial state is null, empty strings, empty []
     const [movie, setMovie] = useState({
         id: null,
         title: "",
@@ -11,9 +15,21 @@ export default function Movie(props) {
         reviews: []
     })
 
+    const getMovie = id => {
+        MovieDataService.get(id).then(res => {
+            setMovie(res.data)
+            console.log(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        getMovie(props.match.params.id)
+    }, [props.match.params.id])
+
     return(
-        <div className="App">
-            Movie
+        <div>
         </div>
     );
 };
