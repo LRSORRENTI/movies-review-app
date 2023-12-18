8da944ff2048a96db5933b34a68fd6d9d08df135
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-18 07:22:30 -0600

    Added container and structure components for movie.jsx

--- frontend/src/components/movie.jsx
+++ frontend/src/components/movie.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import MovieDataService from '../services/movies.js';
 import Link from 'react-router-dom';
 
+
+import {Card, Container, Image, Col, Row, Button, Media} from 'react-bootstrap' 
+
 export default function Movie(props) {
     
     // first define the movie state var to house 
@@ -16,6 +19,9 @@ export default function Movie(props) {
     })
 
     const getMovie = id => {
+        // the method calls get of the MovieDataService//
+        // which is what callsthe API route on the backend
+
         MovieDataService.get(id).then(res => {
             setMovie(res.data)
             console.log(res.data)
@@ -23,13 +29,21 @@ export default function Movie(props) {
             console.log(err)
         })
     }
-
+    // then useEffect calls getMovie every time the 
+    // value for props.match.params.id changes
     useEffect(() => {
         getMovie(props.match.params.id)
     }, [props.match.params.id])
-
+    
     return(
         <div>
+            <Container>
+                <Row>
+                    <Col>
+                    <Image src={movie.poster+"/100pxx250"} fluid />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
