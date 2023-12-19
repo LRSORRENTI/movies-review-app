b533c8f01e2a54231e7092ca4591a5eed48a85aa
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-19 09:34:10 -0600

    Added setup for login component

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+/* 
+NOTE: Not doing full authentication, might add 
+ways to protect routes, salt and hash passwords,
+authorization, resetting passwords, CSRF protection,
+sessions etc.. later
 
-export default function Login() {
+*/
+
+
+import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default function Login(props) {
+    const [name, setName] = useState("")
+    const [id, setId] = useState("")
+
+    const onChangeName = (evt) => {
+        const name = evt.target.value;
+        setName(name)
+    }
+
+    const onChangeID = (evt) => {
+        const id = evt.target.value;
+        setId(id)
+    }
+
+    const login = () => {
+        props.login({name: name, id: id})
+        props.history.push('/')
+    }
     return (
-        <div className="App">
-            Login
+        <div>
+            <Form>
+                <Form.Group>
+                    
+                </Form.Group>
+            </Form>
         </div>
     );
 };
