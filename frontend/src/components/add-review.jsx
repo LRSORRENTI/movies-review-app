b9955a2bb7e10d1a3af26219909610ee0d7625e5
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-20 09:04:12 -0600

    added setup for adding reviews in add-review.jsx

--- frontend/src/components/add-review.jsx
+++ frontend/src/components/add-review.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import MovieDataService from '../services/movies';
+import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 
-export default function AddReview() {
+ const AddReview = props => {
+    let editing = false;
+    let intialReviewState = "";
+
+    const [review, setReview] = useState(intialReviewState);
+    const [submitted, setSubmitted] = useState(false);
+
+    const onChangeReview = evt => {
+        const review = evt.target.value;
+        setReview(review);
+    }
+
     return (
         <div className="App">
             Add Review
         </div>
     );
 };
+
+export default AddReview;
